40f1c28313a3ff837b8ba7bb3e3fb672a1ad3d52
Herick Freitas <herickfreitas@gmail.com>
2021-02-18 17:50:56 -0300

    SQL

---
+++ datasets/SQL/_Fluig_GED_TMOV.sql
@@ -0,0 +1,19 @@
+USE [corporerm]
+GO
+
+/****** Object:  View [dbo].[_Fluig_GED_TMOV]    Script Date: 18/02/2021 17:16:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE VIEW [dbo].[_Fluig_GED_TMOV]
+
+AS
+
+SELECT SUBSTRING(CHAVERM,3,10) AS IDMOV , * FROM HCINTEGRACAOGED where DATASERVER='MovMovimentoData'
+GO
+
